c7949c2050de41940d124177ac95b389d12550db
Stéphane Nguyen <stephane.nguyen@ffe.com>
2022-12-31 15:12:15 +0100

    first commit

---
+++ src/Entity/Picture.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PictureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PictureRepository::class)
+ */
+class Picture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $filename;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Property::class, inversedBy="pictures")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $property;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): self
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    public function getProperty(): ?Property
+    {
+        return $this->property;
+    }
+
+    public function setProperty(?Property $property): self
+    {
+        $this->property = $property;
+
+        return $this;
+    }
+}
